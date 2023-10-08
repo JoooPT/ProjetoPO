3efca47785c265d7171f66283493c468b585e09b
David Antunes <davaantunes@gmail.com>
2023-10-08 19:22:50 +0100

    added things to cell class

--- xxl/core/Cell.java
+++ xxl/core/Cell.java
@@ -2,4 +2,39 @@ package xxl.core;
 
 public class Cell {
     
+    private int _row;
+    private int _column;
+    private Content _content;
+
+    /** 
+     * 
+     */
+    public Cell(int row, int column) {
+        _row = row;
+        _column = column;
+    }
+
+    /**
+     * 
+     * @returns the coordinates of the cell as a String
+     */
+    public String toString() {
+        return "" + _row + ";" + _column;
+    }
+
+    /**
+     * 
+     * @param content that is inserted in the cell
+     */
+    void setContent(Content content) {
+
+    }
+
+    /**
+     * 
+     * @returns the content of the cell as a literal
+     */
+    Literal value() {
+
+    }
 }
